402ae6b4ac67f91ce989dbc8ab1638b851b91e64
aaron <aaronpaulcatabas@gmail.com>
2025-08-26 18:16:44 +0800

    initial commit

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+  default = "./pets.txt"
+}
+variable "content" {
+  default = "We love pets! Weeeee?"
+}
+variable "prefix" {
+  default = "Mrs"
+}
+variable "separator" {
+  default = "."
+}
+variable "length" {
+  default = 2
+}
